5323ff985e1e441f4c13bc19be55f23adf9fecf7
wmujahid <wmujahid47@gmail.com>
2022-02-15 13:31:37 -0500

    added city component and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CityComponent } from './city/city.component';
 import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'city',
+    component: CityComponent
   }
 ];
 
