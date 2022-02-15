ceb0798477c5723278dd26ea01347ce50d1933df
Christopher Fanning <chris@christopherfanning.com>
2022-02-15 15:08:24 -0600

    Add logo to navbar.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
+  cities = [
+    {name: "St louis",
+
+    },
+    {
+      name: "Chicago"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
