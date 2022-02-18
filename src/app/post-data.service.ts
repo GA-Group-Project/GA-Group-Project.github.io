6d33c1627449a9983ecea1884b8bc7b5bc1d4184
Christopher Fanning <chris@christopherfanning.com>
2022-02-18 09:33:46 -0600

    adding the postData service

---
+++ src/app/post-data.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { POSTS } from "./post/posts";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PostDataService {
+
+  posts = POSTS;
+
+  constructor() { }
+
+  getPosts(){
+    return this.posts;
+  }
+
+  addPost(post: any){
+    this.posts.push(post) ;
+  }
+
+  sortArray(arr: any): any{
+    this.posts.sort(function(a,b){
+      // Turn your strings into dates, and then subtract them
+      // to get a value that is either negative, positive, or zero.
+      // @ts-ignore
+      return  new Date(b.date) - new Date(a.date);
+    });
+  }
+}
