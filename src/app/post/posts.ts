d6cbbc64cf01f6a2b575555d4dcea808a45e2c1e
liwren <huangli1223@gmail.com>
2022-02-22 10:08:29 -0600

    changed images

--- src/app/post/posts.ts
+++ src/app/post/posts.ts
@@ -1,7 +1,7 @@
 export const POSTS = [
   {
     id: 0,
-    "img": "https://st3.depositphotos.com/1001146/14087/i/600/depositphotos_140879866-stock-photo-aerial-view-of-miami-beach.jpg",
+    "img": "https://media.istockphoto.com/photos/london-symbols-with-big-ben-double-decker-buses-and-red-phone-booth-picture-id1294454411?b=1&k=20&m=1294454411&s=170667a&w=0&h=UOVuoyqF8H9J3Q5u-HxrWQFRQVQnGgcoF_L8SVbhxQQ=",
     "title": "London.",
     content: "London is great",
     cityId: 0,
@@ -12,7 +12,7 @@ export const POSTS = [
 
   {
     id: 1,
-    "img": "https://st3.depositphotos.com/1001146/14087/i/600/depositphotos_140879866-stock-photo-aerial-view-of-miami-beach.jpg",
+    "img": "https://st2.depositphotos.com/1006832/5234/i/600/depositphotos_52340703-stock-photo-the-scenery-of-sydney-opera.jpg",
     "title": "Sydney opera house!",
     content: "Sydney",
     cityId: 1,
@@ -22,7 +22,7 @@ export const POSTS = [
 
   {
     id: 2,
-    "img": "https://st3.depositphotos.com/1001146/14087/i/600/depositphotos_140879866-stock-photo-aerial-view-of-miami-beach.jpg",
+    "img": "https://media.istockphoto.com/photos/aerial-view-of-san-franciscos-financial-district-on-sunny-day-picture-id1161201567?k=20&m=1161201567&s=612x612&w=0&h=jb2RqT-2IAJxJ1lzzMxkiXiZSfL0viMEhB1S-eD3nUY=",
     "title": "SF is awesome!",
     content: "SF ",
     cityId: 2,
@@ -33,7 +33,7 @@ export const POSTS = [
 
   {
     id: 3,
-    "img": "https://st3.depositphotos.com/1001146/14087/i/600/depositphotos_140879866-stock-photo-aerial-view-of-miami-beach.jpg",
+    "img": "https://media.istockphoto.com/photos/space-needle-and-seattle-downtown-picture-id507294292?k=20&m=507294292&s=170667a&w=0&h=2GBQlp7o3JJQoAZrRjY2qDgIsxtHDvnH13vClToisZw=",
     "title": "Seattle is the best. ",
     content: "Seattle! Rainy",
     cityId: 3,
@@ -43,7 +43,7 @@ export const POSTS = [
 
   {
     id: 4,
-    "img": "https://st3.depositphotos.com/1001146/14087/i/600/depositphotos_140879866-stock-photo-aerial-view-of-miami-beach.jpg",
+    "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bG9uZG9ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
     "title": "Fundon! great!",
     content: "FUndon",
     cityId: 0,
@@ -53,7 +53,7 @@ export const POSTS = [
 
   {
     id: 5,
-    "img": "https://st3.depositphotos.com/1001146/14087/i/600/depositphotos_140879866-stock-photo-aerial-view-of-miami-beach.jpg",
+    "img": "https://st.depositphotos.com/1900091/3887/i/600/depositphotos_38871241-stock-photo-view-of-the-gibraltar-rock.jpg",
     "title": "Gibraltar is ....",
     content: "My trip to Gibraltar was interesting to say the least....",
     cityId: 4,
